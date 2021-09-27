335c6ebb29be77e73e049a264e28c783321cc04f
SEOJEEWOONG <tjwldnd94@naver.com>
2021-09-27 22:25:15 +0900

    Update : React Template

---
+++ README.md
@@ -0,0 +1,8 @@
+# Welcome to LandHeaven
+
+LandHeaven은 사용자와 공인중개사가 소통하는 플랫폼입니다.
+LandHeaven is a platform where users and accredited brokers can communicate
+
+# Tech Stack
+1. React
+2. NodeJs
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
+function App() {
+  return (
+    <div className="card-body">
+      <h1>Welcome to LandHeaven</h1>
+    </div>
+  );
+}
+
+export default App;
